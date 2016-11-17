c8ba10d8cbf10a79d0b3306267f61ec23d30366b
Clalissa <cmy24@uw.edu>
2016-11-17 15:06:39 -0800

    exercise guest lecture

--- ggplot/ggplot_exercise.r
+++ ggplot/ggplot_exercise.r
@@ -13,9 +13,13 @@ suicides <- suicides %>%
   group_by(year, age, sex) %>% 
   mutate(deaths = sum(deaths))
 
+
 #  Make a line plot of suicides by age
 # (year on the x axis, deaths on the y axis, different line for each age).
-# facet by sex.
+# Facet by sex
+line_plot <- ggplot(suicides, aes(x=year, y=deaths, color=age)) +
+  geom_line(size=3) +
+  facet_wrap(~sex, scales="free")
 
 
 ##extra credit####
@@ -25,9 +29,14 @@ one_state <- all_suicides[all_suicides$state=="Uttar Pradesh"] %>%
   mutate(deaths = sum(deaths))
 
 # Make a set of density plots faceted by sex and means of suicide,
-# showing distributions of suicides by age, for the state of Uttar Pradesh.
+# showing distributions of suicides by age, for the state of Uttar Pradesh
 # Label appropriately.
-
+density_plots <- ggplot(one_state, aes(x=deaths)) +
+  geom_density(aes(color=age, fill=age), size=1, alpha=0.5) +
+  facet_grid(means~sex, scales="free") +
+  labs(title="Distribution of Suicides by Age, Sex, and Means, Uttar Pradesh, 2001-2010",
+       x="Deaths",
+       y="Density")
 
 
 
